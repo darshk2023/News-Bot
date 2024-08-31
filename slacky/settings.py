5f2059c024db6d64d2e720d72dddaeb1ad483978
Darsh Khandelwal <darshk@seas.upenn.edu>
2024-08-31 14:25:46 -0400

    Update settings.py

--- slacky/settings.py
+++ slacky/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#-ohv&+9c5ndbnyun3&=doe=yglg+^mq5xi&tn*l3z74%ire)z'
+SECRET_KEY = 'HIDDEN'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
